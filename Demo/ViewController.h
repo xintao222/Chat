97a588cfa3056363b904a2e87a5517d65089f1a3
Jiao <694121590@qq.com>
2013-11-21 11:07:42 +0800

    Revert "..."
    This reverts commit 462b7d423e23690468841ec02de8e663bc1d0d0f.

--- Demo/ViewController.h
+++ Demo/ViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController <UITextFieldDelegate,UITableViewDataSource,UITableViewDelegate>
+@interface ViewController : UIViewController
 
 @end
